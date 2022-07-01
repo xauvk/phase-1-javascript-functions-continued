ffd6ed837a0e9df269100ab0ae0f97fb0b7a1f4d
xauvk <xauvkubalexanderyang@live.com>
2022-07-01 12:07:22 -0400

    commit tree 0a877e5fa5ff8924e53c7a4f7af1ff9388e16d78

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adj = 'special') {
+        return `You are ${flair}${adj}${flair}!` 
+    }
+}
